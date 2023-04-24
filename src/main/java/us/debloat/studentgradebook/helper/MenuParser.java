c0034604a7d506f01a760715371e5d92416aacf6
Miguel <miguel@debloat.us>
2023-04-24 08:33:16 -0400

    before presentation

--- src/main/java/us/debloat/studentgradebook/helper/MenuParser.java
+++ src/main/java/us/debloat/studentgradebook/helper/MenuParser.java
@@ -13,25 +13,21 @@ public class MenuParser {
 		System.out.println(tableBuilder
 				.addFullBorder(BorderStyle.air)
 				.addHeaderBorder(BorderStyle.oldschool)
-				.build().render(100));
+//			.addFullBorder(BorderStyle.fancy_light)
+//			.addHeaderBorder(BorderStyle.fancy_heavy)
+			.build().render(100));
 	}
+
 	public static String fourParameter(int index, String title, String rating, Integer count) {
 		int indexAndTitleLength = 32;
 		StringBuilder result = new StringBuilder();
-		result.append("|")
-		      .append(" ".repeat(index < 10 ? 1 : 0)) //no more than 99 movies
-		      .append(index).append(". ")
-		      .append(title);
+		result.append("|").append(" ".repeat(index < 10 ? 1 : 0)) //no more than 99 movies
+		      .append(index).append(". ").append(title);
 
 		int spacesToAppend = indexAndTitleLength - result.length();
 		if (spacesToAppend > 0) result.append(" ".repeat(spacesToAppend));
 		else result.setLength(indexAndTitleLength);
-		result.append(" ")
-		      .append(rating)
-		      .append(" ".repeat(count > 9 ? 5 : 6))
-		      .append(count)
-		      .append(" ".repeat(5))
-		      .append('|');
+		result.append(" ").append(rating).append(" ".repeat(count > 9 ? 5 : 6)).append(count).append(" ".repeat(5)).append('|');
 		return String.valueOf(result);
 	}
 }
